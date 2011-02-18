a45f96be7769004fcb6073d032711679d38bf6b3
thierry <thierry.henrio@gmail.com>
2011-02-18 21:01:41 +0100

    red, first

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,9 @@
 $LOAD_PATH.unshift(File.expand_path('../app', File.dirname(__FILE__)))
-require 'tinisculus'
 require 'rspec'
 require 'rack/test'
 require 'wrong/adapters/rspec'
 
-set(:environment, :test)
-
 RSpec.configure do |conf|
   conf.include Rack::Test::Methods
-end
-
-def app
-  Tinisculus.new
 end
 
--- spec/tinisculus_spec.rb
+++ spec/tinisculus_spec.rb
@@ -1,8 +1,17 @@
 require 'spec_helper'
+require 'tinisculus'
 
-describe 'The TinisculusApp' do
-  it 'has tinisculus writen' do
-    get '/'
-    assert {last_response.body =~ /Tinisculus/}
+# did not succeed in using set(:environment, :test) with app being a Sinatra::Base
+ENV['RACK_ENV'] = 'test'
+def app
+  Tinisculus
+end
+
+describe Tinisculus do
+  describe "get /" do
+    it 'redirect to /challenge/1' do
+      get '/'
+      assert {last_response.location == 'challenge/1'}
+    end
   end
 end
